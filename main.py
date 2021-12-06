1295ac90b9915b7da02668339c141de8c74e59db
Kovaev <eugenkoval1010@gmail.com>
2021-12-06 19:05:35 +0200

    last final

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import eel
+from pyowm import OWM
 
-eel.init("web")
+owm = OWM("6ac92146bd99b3fb3af69bb9eb99027a")
 
+@eel.expose
+def get_weather(place):
+    
+    mgr = owm.weather_manager()
+
+    observation = mgr.weather_at_place(place)
+    w = observation.weather
+
+    temp = w.temperature('celsius')['temp']
+    return "В місті " + place + " на даний момент " + str(temp) + " градусів!"
+
+eel.init("./web/")
 eel.start("main.html", size=(700,700))
